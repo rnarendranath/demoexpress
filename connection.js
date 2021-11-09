3a0553f1afa6dadf8eff7740043851918e4246d6
Naren <72463822+rnarendranath@users.noreply.github.com>
2021-11-09 11:40:17 +0530

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const ConnectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://naren:naren@cluster0.0p7hn.mongodb.net/mydb?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://naren1:password@cluster0.0p7hn.mongodb.net/mydb?retryWrites=true&w=majority");
 };
 module.exports=ConnectDB;
